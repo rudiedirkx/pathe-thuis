69a7c0d7a8295143f9099182553764180fbb9506
Rudie Dirkx <github@hotblocks.nl>
2023-12-28 00:31:28 +0100

    favicon

--- tpl.header.php
+++ tpl.header.php
@@ -12,6 +12,9 @@ header('Content-type: text/html; charset=utf-8');
 	<meta name="referrer" content="no-referrer" />
 	<title>Pathe Thuis Tracker</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="icon" type="image/png" href="/favicon-128.png" sizes="128x128" />
+	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 	<style>
 	html {
 		font-family: sans-serif;
